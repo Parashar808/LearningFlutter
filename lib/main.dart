75e9864eec3c57f0aee72625adf82cbeabcb36e6
Parashar808 <baralpara4@gmail.com>
2024-03-14 23:30:07 +0545

    added like button, made like and dislike count

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,29 @@ void main() {
   runApp(const MyApp());
 }
 
+
+class Post{
+  String body;
+  String author;
+  int likes=0;
+  bool userLiked=false;
+
+  Post(this.body,this.author);
+
+  void likePost(){
+    this.userLiked = !this.userLiked; //like lai unlike garne,vise-versa
+    
+    if(this.userLiked){
+      this.likes +=1;
+    
+    }
+    else{
+      this.likes -=1;
+    }
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -28,11 +51,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text="";
+  List<Post> posts = [];
 
-  void ChangeText(String text){
+  void newPost(String text){
     this.setState(() {
-      this.text=text;
+     posts.add(new Post(text, "Parashar"));
     });
   }
 
@@ -40,11 +63,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World!'),
+      appBar: AppBar( 
+        title: Text('Hobbies'),
         backgroundColor: Colors.amberAccent,
       ),
-      body:Column(children:<Widget>[TextInputWidget(this.ChangeText),Text(this.text)])
+      body:Column(children:<Widget>[
+       Expanded(child:PostList(this.posts)),
+       TextInputWidget(this.newPost)
+       ])
     );
   }
 }
@@ -70,6 +96,7 @@ class _TextInputWidgetState extends State<TextInputWidget> {
   void click(){
       widget.callback(_controller.text);
       _controller.clear();
+      FocusScope.of(context).unfocus();
 
 
 
@@ -82,13 +109,60 @@ class _TextInputWidgetState extends State<TextInputWidget> {
           controller: _controller,
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.message),
-            labelText: "Type a message",
+            labelText: "Type stuffs",
             suffixIcon: IconButton(
               icon: Icon(Icons.send),
               splashColor: Colors.amber[600],
-              tooltip: "Post Message!",
+              tooltip: "Post this Stuff!",
               onPressed: this.click,
             ))    
+    );
+  }
+}
+
+
+class PostList extends StatefulWidget {
+  
+
+  final List<Post> listItems;
+  PostList(this.listItems);
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callback){
+    this.setState(() {
+      callback();
+    });
+
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context,index){
+        var post = this.widget.listItems[index];
+        return Card(
+          child:Row(
+            children: <Widget>[Expanded(child: ListTile(title: Text(post.body),subtitle: Text(post.author)),
+            ),
+            Row(children: <Widget>[
+              Container(child: Text(post.likes.toString(),style: TextStyle(fontSize: 20)),
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              ),
+              IconButton(
+                onPressed: ()=> this.like(post.likePost),
+                icon: Icon(Icons.thumb_up),
+                color: post.userLiked ? Colors.blue :Colors.black,
+                
+                )],)
+            ],
+          )
+        );
+      },
     );
   }
 }
